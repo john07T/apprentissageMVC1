d21f2a6feb78c9a17c3b1f641e96e343130c7e94
john07T <johnmenahentientcheu@gmail.com>
2025-12-02 19:46:25 +0100

    change4

--- app/Views/home/acceuil.php
+++ app/Views/home/acceuil.php
@@ -6,6 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><b>Bienvenu dans votre Application de reservation de taxi</b></h1>
+
+<!--voici l'entete de la page d'accueil-->
+    <div class="entete">
+        <h2>reservation taxi</h2>
+        <div class="boutton">
+            <a href="#">Accueil</a>
+            <a href="#">Par rapportt</a>
+            <a href="#">Connecter</a>
+            <a href="#">Autentifier vous</a>
+        </div>
+    </div>
+    <!--La partie du corps  de la page d'accueil-->
+    <div class="corps">
+        <div class="miniform">
+            <h1>Bienvenu dans votre application de reservation de taxi</h1>
+            <form action="post">
+                <input type="text" name="disposi" id="" placeholder="Localisation de prise en charge"><br>
+                <input type="text" name="" id="" placeholder="Localisation de depot"><br>
+                <input type="submit" value="Envoyer votre demande">
+            </form>
+        </div>
+        <div class="image1">
+            <img src="app/taxiAc.png">
+        </div>
+    </div>
 </body>
 </html>
