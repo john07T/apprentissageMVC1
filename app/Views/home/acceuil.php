a0c1ced6cea15d78c3305e206e4c7827ed5a011f
john07T <johnmenahentientcheu@gmail.com>
2025-12-09 14:49:06 +0100

    change4

--- app/Views/home/acceuil.php
+++ app/Views/home/acceuil.php
@@ -7,17 +7,16 @@
     <link rel="stylesheet" href="../asset/accueil.css">
 </head>
 <body>
+    <script src="../js/accueill.js"></script>
 
 <!--voici l'entete de la page d'accueil-->
     <div class="entete">
         <h2>reservation taxi</h2>
         <div class="boutton">
             <a href="#">Accueil</a>
-            <a href="#">Par rapportt</a>
+            <a href="#propos">Par rapport</a>
             <a href="#">Connecter</a>
-            <div class="particularButton">
                 <a href="#">Autentifier vous</a>
-            </div>
         </div>
     </div>
     <!--La partie du corps  de la page d'accueil-->
@@ -25,14 +24,47 @@
         <div class="miniform">
             <h1>Bienvenu dans votre application de reservation de taxi</h1>
             <form action="post">
-                <input type="text" name="disposi" id="" placeholder="Localisation de prise en charge"><br>
-                <input type="text" name="" id="" placeholder="Localisation de depot"><br>
-                <input type="submit" value="Envoyer votre demande">
+                <table>
+                    <tr>
+                        <td><input type="text" name="disposi" id="" placeholder="Localisation de prise en charge"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="" id="" placeholder="Localisation de depot"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Envoyer votre demande" height=100px></td>
+                    </tr>
+                </table>
             </form>
         </div>
         <div class="image1">
-            <img src="app/taxiAc.png">
+            <img src="../asset/taxiAc.png">
         </div>
     </div>
+<!-- parite de la disposition des cartes -->
+<div class="texte1"><h1>A propos de l'application</h1></div>
+    <div id="propos">
+    <div class="info">
+        <div class="card box1">
+            <div class="photo">
+                <img src="../asset/taxiAc.png" alt="">
+            </div>
+            <div class="text">tres fiable et securise</div>
+        </div>
+        <div class="card box2">
+            <div class="photo">
+                <img src="../asset/taxiAc.png" alt="">
+            </div>
+            <div class="text">paiement fiable</div>
+        </div>
+        <div class="card box3">
+            <div class="photo">
+                <img src="../asset/taxiAc.png" alt="">
+            </div>
+            <div class="text">paiement fiable</div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
---
+++ app/Views/js/accueill.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll',()=>{ 
+const box1=document.querySelector(".box1");
+const box2=document.querySelector(".box2");
+const box3=document.querySelector(".box3");
+
+
+box1.classList.add('animateUp');
+box2.classList.add('animateDown');
+box3.classList.add('animateUp1');
+
+
+
+})
